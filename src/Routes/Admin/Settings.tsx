f0611e1e72d63b1ce922a9f42920c76e6969418d
ethan <ethan.fifle@gmail.com>
2024-03-18 00:03:01 -0400

    Bearer token

--- src/Routes/Admin/Settings.tsx
+++ src/Routes/Admin/Settings.tsx
@@ -1,64 +1,80 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PostAccount from "../../Services/PostAccount";
-import { useAccountData } from "../../Hooks/AccountData";
-import { useFormContext } from "../../Context/LocalObjectForm";
+import {useAccountData} from "../../Hooks/AccountData";
+import {useFormContext} from "../../Context/LocalObjectForm";
+import { useAuth } from '@clerk/clerk-react';
 
 const Settings = () => {
 
-    const { formData, updateFormData } = useFormContext();
-    const apiData = useAccountData(formData);
+  const {formData, updateFormData} = useFormContext();
+  const apiData = useAccountData(formData);
+  const auth = useAuth();
+  const [token, setToken] = useState('');
 
-    return (
-      <div className="flex flex-col bg-custom-bg h-screen justify-center items-center mt-16 md:mt-0 md:ml-64 p-2">
+  useEffect(() => {
+    const fetchToken = async () => {
+        const jwt = await auth.getToken({template: 'SitemasterBE'});
+        if (jwt !== null) {
+          setToken(jwt);
+        } else {
+          setToken(''); // No valid token
+        }
+    };
 
-          <div className="text-left mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="site-name">
-                  Name:
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="site-name"
-                type="text"
-                value={formData.name || ''}
-                onChange={(e) => updateFormData({name: e.target.value})}
-                placeholder="name..."/>
-          </div>
+    fetchToken().then(() => console.log(`new token fetched: ${token}`));
+  }, []);
 
+  return (
+    <div className="flex flex-col bg-custom-bg h-screen justify-center items-center mt-16 md:mt-0 md:ml-64 p-2">
 
-          <div className="text-left mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="site-name">
-                  Phone number:
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="site-location"
-                type="text"
-                value={formData.phoneNumber || ''}
-                onChange={(e) => updateFormData({phoneNumber: e.target.value})}
-                placeholder="phone number..."/>
-          </div>
-
-          <div className="text-left mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="site-name">
-                  Email:
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="site-location"
-                type="text"
-                value={formData.email || ''}
-                onChange={(e) => updateFormData({email: e.target.value})}
-                placeholder="email..."/>
-          </div>
-
-          <div
-            className="bg-white w-36 text-custom-grey rounded-[5px] drop-shadow">
-              <button className="p-2" onClick={() => PostAccount(apiData)}>
-                  Post Account
-              </button>
-          </div>
+      <div className="text-left mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="site-name">
+          Name:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="site-name"
+          type="text"
+          value={formData.name || ''}
+          onChange={(e) => updateFormData({name: e.target.value})}
+          placeholder="name..."/>
       </div>
-    );
+
+
+      <div className="text-left mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="site-name">
+          Phone number:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="site-location"
+          type="text"
+          value={formData.phoneNumber || ''}
+          onChange={(e) => updateFormData({phoneNumber: e.target.value})}
+          placeholder="phone number..."/>
+      </div>
+
+      <div className="text-left mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="site-name">
+          Email:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="site-location"
+          type="text"
+          value={formData.email || ''}
+          onChange={(e) => updateFormData({email: e.target.value})}
+          placeholder="email..."/>
+      </div>
+
+      <div
+        className="bg-white w-36 text-custom-grey rounded-[5px] drop-shadow">
+        <button className="p-2" onClick={() => PostAccount(apiData, token)}>
+          Post Account
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
--- src/Services/PostAccount.ts
+++ src/Services/PostAccount.ts
@@ -1,26 +1,27 @@
-import { Account } from "../Utility/GlobalTypes";
+import {Account} from "../Utility/GlobalTypes";
 
-const postAccount = async (apiData: Account): Promise<string | null> => {
-    try {
-        const response = await fetch(`${process.env.REACT_APP_BE_URL}/Client`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(apiData)
-        });
+const postAccount = async (apiData: Account, token: string): Promise<string | null> => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_BE_URL}/Client`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(apiData)
+    });
 
-        if (!response.ok) {
-            throw { status: response.status, message: `HTTP error! Status: ${response.status}` };
-        }
-
-        const data = await response.json();
-        // use FE Logger when set up ('Profile Posted Successfully')
-        return data.id;
-    } catch (error: any) {
-        // use FE Logger when set up ('Error posting profile data')
-        return null;
+    if (!response.ok) {
+      throw {status: response.status, message: `HTTP error! Status: ${response.status}`};
     }
+
+    const data = await response.json();
+    // use FE Logger when set up ('Profile Posted Successfully')
+    return data.id;
+  } catch (error: any) {
+    // use FE Logger when set up ('Error posting profile data')
+    return null;
+  }
 };
 
 export default postAccount;
